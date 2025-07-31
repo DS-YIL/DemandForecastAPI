a38a0ec9dee8e0d3f1d1cee252cb573b378569b9
yokogawadeveloper <ankul.gautam@yokogawa.com>
2025-07-31 10:47:01 +0530

    old code without enhacement

---
+++ datas/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+
+@admin.register(inputFromUi)
+class inputFromUiAdmin(admin.ModelAdmin):
+    list_display = ('id', 'threshold','pipelineWeek','requiredWeek','inventoryTarget')
+
+@admin.register(SectorWise)
+class SectorWisedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'consumedQty', 'projectedQty')
+
+@admin.register(InventoryGraph)
+class InventoryGraphdAdmin(admin.ModelAdmin):
+    list_display = ('date', 'CPA110Y', 'CPA430Y','CPA530Y', 'CPA_total', 'CPA_Cost','KDP_Cost','Total_Inventory', 'CPA_Receipt')
